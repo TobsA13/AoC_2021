8d76c19226347b01cf898e2e54c1e6c3e13bc675
Tobias Albert <tobias.albert@hetzner.com>
2021-12-01 07:15:07 +0100

    Day 1

---
+++ 01.py
@@ -0,0 +1,41 @@
+from pprint import pprint
+
+print("AoC 2021-01")
+print()
+
+f = open("01data", "r")
+data = f.readlines()
+
+#------------- Part 1
+
+count = 0
+old_count = 0
+
+for i, d in enumerate(data):
+    if i == 0:
+        continue
+    if int(d) > old_count:
+        count += 1
+    old_count = int(d)
+
+print("Result: " + str(count))
+
+#------------- Part 2
+
+cleanData = []
+for i in range(len(data)):
+    if len(data) - 2 == i:
+        break
+    cleanData.append(int(data[i]) + int(data[i + 1]) + int(data[i + 2]))
+
+count = 0
+old_count = 0
+
+for i, d in enumerate(cleanData):
+    if i == 0:
+        continue
+    if int(d) > old_count:
+        count += 1
+    old_count = int(d)
+
+print("Result 2: " + str(count))
